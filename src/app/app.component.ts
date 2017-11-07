c2bbe592a840a11769bc4e1155e6b3bc1bea2093
Daren Keck <darenkeck@gmail.com>
2017-11-06 20:40:23 -0800

    Connect 'new jumble' to jumble service command

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Http }      from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+
 import { FbaseService } from 'app/services/fbase.service';
+import { JumbleService } from './services/jumble.service';
 
 // enum to help track current tab
 export const TabPage = {
@@ -20,7 +22,11 @@ export class AppComponent {
   title = 'Daren Keck';
   tab = TabPage.HOME;
 
-  constructor(private fbase: FbaseService) {
+  constructor(private fbase: FbaseService, private jumbleService: JumbleService) {
     this.entryList = this.fbase.fetchList('blog-entries');
+  }
+
+  newJumble() {
+    this.jumbleService.setJumble();
   }
 }
